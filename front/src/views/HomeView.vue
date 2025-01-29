2254e9a853a75e3a9b1d887af05c58ae0a2b7626
YCJKen <jiangyucheng2019@163.com>
2025-01-30 05:28:16 +0800

    前端重构
    将offer功能集成到一个按键之中

--- front/src/views/HomeView.vue
+++ front/src/views/HomeView.vue
@@ -1,35 +1,54 @@
 <template>
     <div class="home">
         <el-card class="welcome-card">
-            <h1>欢迎来到主页</h1>
+            <h1>{{ showOfferContent ? 'OfferShow' : '欢迎来到主页' }}</h1>
             <div class="button-group">
-                <el-button type="primary" @click="$router.push('/offers')">
-                    查看我的 Offers
-                </el-button>
-                <el-button type="success" @click="$router.push('/offers/create')">
-                    创建新的 Offer
-                </el-button>
+                <template v-if="!showOfferContent">
+                    <el-button type="primary" @click="handleShowOffer">
+                        OfferShow
+                    </el-button>
+                </template>
+                <template v-else>
+                    <el-button type="primary" @click="$router.push('/offers')">
+                        查看我的 Offers
+                    </el-button>
+                    <el-button type="success" @click="$router.push('/offers/create')">
+                        创建新的 Offer
+                    </el-button>
+                    <el-button type="info" @click="handleBack">
+                        返回主界面
+                    </el-button>
+                </template>
                 <el-button type="warning" @click="handleBackToLogin">
                     返回登录页
                 </el-button>
             </div>
         </el-card>
 
-        <!-- 添加推荐 Offers 组件 -->
-        <RecommendedOffers />
+        <RecommendedOffers v-if="showOfferContent" />
     </div>
 </template>
 
 <script setup>
+import { ref } from 'vue'
 import { useRouter } from 'vue-router'
 import RecommendedOffers from '../components/RecommendedOffers.vue'
 import { removeToken } from '../utils/auth'
 
 const router = useRouter()
+const showOfferContent = ref(false)
 
 const handleBackToLogin = async () => {
     removeToken()
     await router.push('/login')
+}
+
+const handleShowOffer = () => {
+    showOfferContent.value = true
+}
+
+const handleBack = () => {
+    showOfferContent.value = false
 }
 </script>
 
@@ -51,5 +70,10 @@ const handleBackToLogin = async () => {
 }
 .button-group .el-button {
     margin: 0 10px;
+}
+
+.home :deep(.el-card),
+.home :deep(.recommended-offers) {
+    transition: all 0.3s ease;
 }
 </style> 
